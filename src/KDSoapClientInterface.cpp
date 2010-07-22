19783d0549d1586e392d2a92a778a87c23002fb3
Kevin Krammer <kevin.krammer@kdab.com>
2010-07-22 13:13:06 +0000

    Some servers seem to use the HTTP encoding over the on specified in the XML processing instruction. Since we are using UTF-8 there, specify it for the HTTP header as well
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@36141 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDSoapClientInterface.cpp
+++ src/KDSoapClientInterface.cpp
@@ -119,7 +119,7 @@ QNetworkRequest KDSoapClientInterface::Private::prepareRequest(const QString &me
 {
     QNetworkRequest request(QUrl(this->m_endPoint));
     // Seems SOAP-1.2 uses application/soap+xml instead of text/xml.
-    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml;charset=utf-8"));
     // The soap action seems to be namespace + method in most cases, but not always
     // (e.g. urn:GoogleSearchAction for google).
     QString soapAction = action;
