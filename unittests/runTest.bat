71b85165b6b87111b978cb72e10c6b8841998cda
David Faure <david.faure@kdab.com>
2010-08-10 23:53:34 +0000

    Strange, I don't get debug\ or release\ subdirs, neither in kdsoap nor kdchart nor kdreports, so I would need this fix in all 3 products for "nmake test" to work for me. I'm not 100% sure but it seems there's no such subdirs even when compiling from the MSVC IDE (I mostly tested using qmake+nmake, but a quick ide test says no subdir either...). Does anyone really get debug\ and release\ subdirs still? Autobuild-on-windows agrees with me, in any case, it seems.
    CCMAIL: patrick@kdab.com, mike@kdab.com
    
    
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@36408 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- unittests/runTest.bat
+++ unittests/runTest.bat
@@ -1,9 +1,11 @@
 @echo off
 
-echo running test %1 in %2 mode
+REM %2 is debug or release, was used as %2\%1.exe, but such subdirs don't seem to exist anymore
+
+echo running test %1
 cd %1
 set OLD_PATH=%PATH%
 set PATH=..\..\lib;%PATH%
-%2\%1.exe
+%1.exe
 set PATH=%OLD_PATH%
 cd ..
