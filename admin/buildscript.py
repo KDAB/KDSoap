c2f6f85098f280af789f1ad2b3673bfde20373d8
David Faure <david.faure@kdab.com>
2010-08-11 14:43:55 +0000

    Port to the 'autotools' builder, i.e. let autobuild run configure.sh. Entirely based on the similary file in kdchart trunk.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@36430 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- admin/buildscript.py
+++ admin/buildscript.py
@@ -6,33 +6,29 @@ from AutobuildCore.helpers.build_script_helpers import DebugN
 from AutobuildCore.helpers.exceptdefs import AutobuildException
 from AutobuildCore import autobuildRoot
 
-# some variables:
-scmPath = 'svn+ssh://svn.kdab.net/home/SVN-klaralv/products/kdsoap'
 buildSequenceSwitches = 'enable-conf-create-installer'
-noDebug = ''
-noRelease = ''
 hiddenFileIdent = '.'
 
 if 'Windows' in platform.platform():
-	noDebug = ' CONFIG-=debug CONFIG-=debug_and_release'
-	noRelease = ' CONFIG-=release CONFIG-=debug_and_release'
 	buildSequenceSwitches += ',disable-conf-bin-package'
 	hiddenFileIdent = '_'
 
+scmPath = 'svn+ssh://svn.kdab.net/home/SVN-klaralv/products/kdsoap'
 product = Project( 'KD Soap' )
-product.setVersionName( '1.1.0' )
-product.setScmUrl(scmPath + '/trunk')
-product.setPackageLocation( 'svn.kdab.net:/home/build/autobuild/packages/kdsoap' )
-product.setBuildSequenceSwitches('s', buildSequenceSwitches)
-product.setBuildSequenceSwitches('f', buildSequenceSwitches)
+
+product.setScmUrl( scmPath + '/trunk' )
+product.setPackageLocation( 'svn.kdab.com:/home/build/autobuild/packages/kdsoap' )
+product.setBuildSequenceSwitches( 's', buildSequenceSwitches )
+product.setBuildSequenceSwitches( 'f', buildSequenceSwitches )
 
 # pre-build hook functions that copy the documentation and the license into the
 # target folder, if they are checked into internal/extra/<configname>:
 def preBuildJobHook( buildJob ):
-        DebugN( 1, 'kdSoap build script: pre-build hook called for build job ' + buildJob.fileSystemName() )
+        DebugN( 1, 'kdsoap build script: pre-build hook called for build job ' + buildJob.fileSystemName() )
 	step = buildJob.executomat().step( 'conf-make-install' )
 	targetDir = os.path.normpath( buildJob.targetDir() )
 	exportTool = autobuildRoot() + os.sep + '..' + os.sep + 'Tools' + os.sep + 'SvnExport.py'
+	buildDir = os.path.normpath(buildJob.buildDir())
 	if not os.path.isfile( exportTool ):
 		raise AutoBuildError( 'cannot find SvnExport.py in Tools/' )
 	# we only copy things in for packaging
@@ -43,63 +39,83 @@ def preBuildJobHook( buildJob ):
 			+ 'internal' + os.sep + 'extra' + os.sep + extrasFolder )
 		cmd = 'python ' + exportTool + ' -f -s ' + extrasPath + ' -t ' + targetDir 
 		step.addPostCommand( cmd, targetDir )
-		# prepare the install folder for the different configurations:
-		if 'Commercial' in buildJob.configuration().getConfigName():
-			# copy source code into install folder for commercial builds:
-                        excludedFiles = ' -d lib,manual,Licenses,config -x g++.pri,kdsoap.dox,.qmake-cache'
-			cmd = 'python ' + exportTool + ' -f -s ' + buildJob.configuration().project().srcDir() + excludedFiles
-			cmd += ' -t ' + targetDir
-			step.addPostCommand( cmd, buildJob.configuration().project().srcDir() )
-			# clean up the source
-                        # (not there in KD Soap, yet)
-			# cmd = 'python ' + buildJob.configuration().project().checkoutDir() \
-			#		+ os.sep + 'admin' + os.sep + 'prepare-source-export.py'
-			#		
-			# step.addPostCommand( cmd, targetDir )
-			# rm the lib directory, the user will compile the source by himself:
-			cmd = 'rm -Rf lib'
-			step.addPostCommand( cmd, targetDir )
-                DebugN( 2, 'kdsoap build script: added command to copy docs and licenses into target folder' )
+		# copy source code into install folder for commercial builds:
+		excludedFiles = ' -d lib,manual,Licenses,config -x g++.pri,kdsoap.dox,.qmake-cache'
+		cmd = 'python ' + exportTool + ' -f -s ' + buildJob.configuration().project().srcDir() + excludedFiles
+		cmd += ' -t ' + targetDir
+		step.addPostCommand( cmd, buildJob.configuration().project().srcDir() )
+		# clean up the source 
+		# (not there in KD Soap, yet)
+		# cmd = 'python ' + buildJob.configuration().project().checkoutDir() \
+		#		+ os.sep + 'admin' + os.sep + 'prepare-source-export.py'
+		# step.addPostCommand( cmd, targetDir )
+		# rm the lib directory, the user will compile the source by himself:
+		cmd = 'rm -Rf lib'
+		step.addPostCommand( cmd, targetDir )
+		DebugN( 2, 'kdsoap build script: added command to copy docs and licenses into target folder' )
+	stepExportSrc = buildJob.executomat().step('conf-export-src')
+	cmd3 = 'touch .license.accepted'
+	stepExportSrc.addPostCommand(cmd3, buildDir)
+	DebugN(2, 'kdsoap build script: added command to copy license into build folder')
 
-SharedDebug = Configuration( product, 'Debug' )
-SharedDebug.setPackageDependencies( ['Qt-4.[4-9].?-Shared-Debug'])
-SharedDebug.setBuildMode( 'inSource' )
-SharedDebug.setPlatFormWhiteList( ['win32-msvc2005', 'macx-g++','linux-g++','linux-g++-64'] )
-SharedDebug.setOptions( 'CONFIG+=unittests CONFIG+=debug' + noRelease )
+product = Project( 'KD Soap' )
+product.setScmUrl( scmPath + '/trunk' )
+product.setPackageLocation( 'svn.kdab.com:/home/build/autobuild/packages/kdsoap' )
+product.setBuildSequenceSwitches( 's', buildSequenceSwitches )
+product.setBuildSequenceSwitches( 'f', buildSequenceSwitches )
+
+SharedDebug = Configuration( product, 'Shared-Debug' )
 SharedDebug.setPreBuildHook( preBuildJobHook )
-SharedDebug.setBuildTypes( 'MCDF' )# debug is not built for snapshots
-# TODO this should be ported to the autotools builder, so that it actually calls configure.sh/configure.bat!
+SharedDebug.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Debug' ] )
+SharedDebug.setBuildMode( 'inSource' )
+#SharedDebug.setPlatFormWhiteList( ['win32-msvc2005', 'macx-g++','linux-g++','linux-g++-64'] )
+SharedDebug.setOptions( '-unittests -debug' )
+SharedDebug.setBuildTypes( 'MCDF' ) # debug is not built for snapshots
 
-comSharedRelease = copy.copy( SharedDebug ) # use comDebug as the base configuration
-comSharedRelease.setConfigName( 'Commercial Release' )
-comSharedRelease.setPackageDependencies( ['Qt-4.[4-9].?-Shared-Release'])
-comSharedRelease.setOptions( 'CONFIG+=release' + noDebug )
-comSharedRelease.setBuildTypes( 'MCDSF' )# snapshots are release builds
+commercial = Configuration( product , 'commercial')
+commercial.setPreBuildHook( preBuildJobHook )
+commercial.setBuilder('autotools')
+commercial.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Release' ] )
+commercial.setBuildMode( 'inSource' )
+commercial.setOptions( '-unittests -release' )
+commercial.setBuildTypes( 'MCDSF' ) # snapshots are release builds
 
-SharedDebugKDChartSupport = copy.copy( SharedDebug)
-SharedDebugKDChartSupport.setConfigName('Debug KDchart Support')
-SharedDebugKDChartSupport.setPackageDependencies( ['Qt-4.[4-9].?-Shared-Debug','KDChart-2.*'])
-SharedDebugKDChartSupport.setBuildTypes( 'MCDF' )# debug is not built for snapshots
+StaticDebug = Configuration( product, 'Static-Debug')
+StaticDebug.setPreBuildHook( preBuildJobHook )
+StaticDebug.setBuilder('autotools')
+StaticDebug.setPackageDependencies( [ 'Qt-4.[4-9].?-Static-Debug' ] )
+StaticDebug.setBuildMode( 'inSource' )
+StaticDebug.setOptions( '-unittests -static -debug' )
+StaticDebug.setBuildTypes('MCDF')
 
-SharedReleaseKDChartSupport = copy.copy( comSharedRelease ) # use comDebug as the base configuration
-SharedReleaseKDChartSupport.setConfigName( 'Release KDChart Support' )
-SharedReleaseKDChartSupport.setPackageDependencies( ['Qt-4.[4-9].?-Shared-Release','KDChart-2.*'] )
-SharedReleaseKDChartSupport.setOptions( 'CONFIG+=release' + noDebug )
+StaticRelease = Configuration( product, 'Static-Release')
+StaticRelease.setPreBuildHook( preBuildJobHook )
+StaticRelease.setBuilder('autotools')
+StaticRelease.setPackageDependencies( [ 'Qt-4.[4-9].?-Static-Release' ] )
+StaticRelease.setBuildMode( 'inSource' )
+StaticRelease.setOptions( '-unittests -static -release' )
+StaticRelease.setBuildTypes('MCDF')
 
-comSharedReleaseUS = copy.copy( comSharedRelease ) # use comRelease as the base configuration
-comSharedReleaseUS.setConfigName( 'Commercial US Release' )
-comSharedReleaseUS.setBuildTypes( 'MSF' )# snapshots are release builds
+commercial_us = Configuration( product, 'commercial_us')
+commercial_us.setPreBuildHook( preBuildJobHook )
+commercial_us.setBuilder('autotools')
+commercial_us.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Release' ] )
+commercial_us.setBuildMode( 'inSource' )
+commercial_us.setOptions( '-unittests -release' )
+commercial_us.setBuildTypes( 'MSF' ) # snapshots are release builds
 
-gplRelease = copy.copy( comSharedRelease )
-gplRelease.setConfigName( 'GPL' )
-gplRelease.setBuildTypes( 'MSF' )# snapshots are release builds
+gpl = Configuration( product, 'gpl')
+gpl.setPreBuildHook( preBuildJobHook )
+gpl.setBuilder('autotools')
+gpl.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Release' ] )
+gpl.setBuildMode( 'inSource' )
+gpl.setOptions( '-unittests -release' )
+gpl.setBuildTypes( 'MSF' ) # snapshots are release builds
 
-jobs = [ SharedDebug, comSharedRelease, SharedDebugKDChartSupport, SharedReleaseKDChartSupport, comSharedReleaseUS, gplRelease ]
-
-def preSetupCallback(project):
-    if len(scmPath) < len(project.getScmUrl()):
-        project.setPackageSubDir(project.getScmUrl()[len(scmPath):])
-product.registerPresetupCallback(preSetupCallback)
+if 'Darwin' in platform.platform():
+	jobs = [ SharedDebug, commercial,commercial_us, gpl ]
+else:
+	jobs = [ SharedDebug, commercial, StaticDebug, StaticRelease, commercial_us, gpl ]
 
 # set up a callback to inject extra command line commands into the build process:
 # in this case, we add a command to be executed in the folder where the source code is executed 
@@ -112,5 +128,10 @@ product.registerPresetupCallback(preSetupCallback)
 #	step = project.postExecutomat().step( 'project-src-package' )
 #	step.addMainCommand( cmd, exportFolder )
 #product.registerPrebuildCallback( prebuildCallback )
+
+def preSetupCallback( project ):
+	if len( scmPath ) < len( project.getScmUrl() ):
+		project.setPackageSubDir(project.getScmUrl()[len( scmPath ):])
+product.registerPresetupCallback(preSetupCallback)
 
 product.build( jobs )
