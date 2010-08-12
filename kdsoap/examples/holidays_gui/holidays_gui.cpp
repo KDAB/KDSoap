e6b3f7ee948860dc6bfb93289fb5119cabec5061
Kevin Krammer <kevin.krammer@kdab.com>
2010-08-12 09:49:22 +0000

    Fix log output: Holiday examples look up Valentine's Day, not easter.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@36465 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- kdsoap/examples/holidays_gui/holidays_gui.cpp
+++ kdsoap/examples/holidays_gui/holidays_gui.cpp
@@ -29,14 +29,14 @@ int main(int argc, char **argv)
 
     const int year = 2009;
 
-    const QString endPoint = QString::fromLatin1("http://www.27seconds.com/Holidays/US/Dates/USHolidayDates.asmx");
-    const QString messageNamespace = QString::fromLatin1("http://www.27seconds.com/Holidays/US/Dates/");
+    const QString endPoint = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/USHolidayDates.asmx");
+    const QString messageNamespace = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/");
     KDSoapClientInterface client(endPoint, messageNamespace);
 
     KDSoapMessage message;
     message.addArgument(QLatin1String("year"), year);
 
-    qDebug("Looking up the date of easter in %i...", year);
+    qDebug("Looking up the date of Valentine's Day in %i...", year);
 
     KDSoapPendingCall pendingCall = client.asyncCall(QLatin1String("GetValentinesDay"), message);
     TestObject test;
--- kdsoap/examples/holidays_sync/holidays.cpp
+++ kdsoap/examples/holidays_sync/holidays.cpp
@@ -11,14 +11,14 @@ int main(int argc, char **argv)
 
     const int year = 2009;
 
-    const QString endPoint = QString::fromLatin1("http://www.27seconds.com/Holidays/US/Dates/USHolidayDates.asmx");
-    const QString messageNamespace = QString::fromLatin1("http://www.27seconds.com/Holidays/US/Dates/");
+    const QString endPoint = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/USHolidayDates.asmx");
+    const QString messageNamespace = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/");
     KDSoapClientInterface client(endPoint, messageNamespace);
 
     KDSoapMessage message;
     message.addArgument(QLatin1String("year"), year);
 
-    qDebug("Looking up the date of easter in %i...", year);
+    qDebug("Looking up the date of Valentine's Day in %i...", year);
 
     KDSoapMessage response = client.call(QLatin1String("GetValentinesDay"), message);
 
