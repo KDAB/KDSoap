d7a8e781ede4b6fd2d8743a1f3bb8136a7af593d
David Faure <david.faure@kdab.com>
2010-08-16 17:42:41 +0000

    remove now-implemented TODO
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@36543 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/holidays_gui/holidays_gui.cpp
+++ examples/holidays_gui/holidays_gui.cpp
@@ -1,13 +1,6 @@
 #include <QApplication>
 #include "mainwindow.h"
 
-
-#include <QDebug>
-
-// TODO a button for an async call, a button for a sync call,
-// and some background operation that will stop working during the sync call
-// (or just a mouseover effect?)
-
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
