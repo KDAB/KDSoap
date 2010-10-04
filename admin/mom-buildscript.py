84db4a9926a56eb4e5f22475cdf5079828f5808d
Mirko Boehm <mirko.boehm@kdab.com>
2010-10-04 15:52:31 +0000

    Add Make-O-Matic build script.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@37208 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ admin/mom-buildscript.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# KDAB confidential
+#
+# This file is part of KDAB's internal build system setup.
+# Author: Mirko Boehm, mirko.boehm@kdab.com
+#
+# This script sets up Make-O-Matic and Autobuild in the current folder.
+# It generates a shell script that can be sourced to use the created environment.
+
+from core.plugins.packagers.CPack import CPack
+from core.helpers.BoilerPlate import getBuildProject
+from products.ProductConfiguration import ProductConfiguration
+
+build, project = getBuildProject( minimumMomVersion = "0.5.0",
+	projectName = "KD SOAP", projectVersionNumber = '1.0.0',
+	scmUrl = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk' )
+
+staticDebug = ProductConfiguration( configName = 'Static Debug', debug = True, shared = False, parent = project )
+staticRelease = ProductConfiguration( configName = 'Static Release', debug = False, shared = False, parent = project )
+sharedDebug = ProductConfiguration( configName = 'Shared Debug', debug = True, shared = True, parent = project )
+sharedRelease = ProductConfiguration( configName = 'Shared Release', debug = False, shared = True, parent = project )
+
+sharedRelease.addPlugin( CPack( sourcePackage = True ) )
+sharedRelease.addPlugin( CPack( sourcePackage = False ) )
+
+build.build()
