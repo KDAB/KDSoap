f7776cbeaf718cc451c90aa1f21a55dda9874645
Frank Osterfeld <frank.osterfeld@kdab.com>
2010-11-18 10:38:33 +0000

    add license blurbs
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.0.0-release@37919 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- kdwsdl2cpp/src/typemap.h
+++ kdwsdl2cpp/src/typemap.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+** Copyright (C) 2010-2010 Klaralvdalens Datakonsult AB.  All rights reserved.
+**
+** This file is part of the KD SOAP library.
+**
+** Licensees holding valid commercial KD Soap licenses may use this file in
+** accordance with the KD Soap Commercial License Agreement provided with
+** the Software.
+**
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.GPL included.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Contact info@kdab.com if any conditions of this licensing are not
+** clear to you.
+**
+**********************************************************************/
+
 /*
     Copyright (c) 2005 Tobias Koenig <tokoe@kde.org>
     Copyright (c) 2010 David Faure <dfaure@kdab.com>
