3eb3a7102a0bd023901dbb2e0921fc30f019bd57
Mike McQuaid <mike.mcquaid@kdab.com>
2010-11-26 15:20:27 +0000

    Update boilerplate and use branch functionality.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.0@38058 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- admin/mom-buildscript.py
+++ admin/mom-buildscript.py
@@ -2,21 +2,21 @@
 #
 # KDAB confidential
 #
-# This file is part of KDAB's internal build system setup.
-# Author: Mirko Boehm, mirko.boehm@kdab.com
+# This file is part of KDAB's internal build system setup. It is the
+# build script for KD SOAP, which integrates it into the continuous
+# integration system.
 #
-# This script sets up Make-O-Matic and Autobuild in the current folder.
-# It generates a shell script that can be sourced to use the created environment.
+# Author: Mirko Boehm, mirko.boehm@kdab.com
 
+
+from core.helpers.BoilerPlate import BuildProject
+from products.ProductBoilerPlate import ProductConfigurations
 from core.plugins.packagers.CPack import CPack
-from core.helpers.BoilerPlate import getBuildProject
-from products.ProductBoilerPlate import getConfigurations
 
-build, project = getBuildProject( minimumMomVersion = "0.5.0",
-	projectName = "KD SOAP", projectVersionNumber = '1.0.0',
-	scmUrl = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.0' )
+build, project = BuildProject( 'KD SOAP', 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap', '1.0.0',
+								branch = "kdsoap-1.0", minimumMomVersion = "0.5.0" )
 
-staticDebug, staticRelease, sharedDebug, sharedRelease = getConfigurations( project )
+staticDebug, staticRelease, sharedDebug, sharedRelease = ProductConfigurations( project )
 
 sharedRelease.addPlugin( CPack( sourcePackage = True ) )
 
