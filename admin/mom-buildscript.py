972641d97f6908f271d8865e0909587dc18e30c6
Mike McQuaid <mike.mcquaid@kdab.com>
2010-11-26 15:15:49 +0000

    Update MOM buildscript with latest boilerplate.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@38057 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- admin/mom-buildscript.py
+++ admin/mom-buildscript.py
@@ -9,15 +9,14 @@
 # Author: Mirko Boehm, mirko.boehm@kdab.com
 
 
+from core.helpers.BoilerPlate import BuildProject
+from products.ProductBoilerPlate import ProductConfigurations
 from core.plugins.packagers.CPack import CPack
-from core.helpers.BoilerPlate import getBuildProject
-from products.ProductBoilerPlate import getConfigurations
 
-build, project = getBuildProject( minimumMomVersion = "0.5.0",
-	projectName = "KD SOAP", projectVersionNumber = '1.0.0',
-	scmUrl = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
+build, project = BuildProject( 'KD SOAP', 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap', '1.0.0',
+								minimumMomVersion = "0.5.0" )
 
-staticDebug, staticRelease, sharedDebug, sharedRelease = getConfigurations( project )
+staticDebug, staticRelease, sharedDebug, sharedRelease = ProductConfigurations( project )
 
 sharedRelease.addPlugin( CPack( sourcePackage = True ) )
 
