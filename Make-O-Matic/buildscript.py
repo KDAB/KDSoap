205438d8edc0fa6629d710286370401685d9c294
Mike McQuaid <mike.mcquaid@kdab.com>
2011-02-11 12:48:44 +0000

    Add Make-O-Matic buildscript for KD Soap branch.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.0@38942 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ Make-O-Matic/buildscript.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# KDAB confidential
+#
+# This file is part of KDAB's internal build system setup. It is the
+# build script for KD SOAP, which integrates it into the continuous
+# integration system. 
+#
+# Author: Mirko Boehm, mirko.boehm@kdab.com
+
+from core.helpers.BoilerPlate import BuildProject
+from products.ProductBoilerPlate import ProductConfigurations
+from products.ProductPackager import ProductPackager
+
+build, project = BuildProject( name = 'KD SOAP', version = '1.0.1', branch = 'kdsoap-1.0'
+    url = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
+
+configs = ProductConfigurations( project )
+
+default = configs.getDefaultConfiguration()
+default.addPlugin( ProductPackager( sourcePackage = True ) )
+
+build.build()
