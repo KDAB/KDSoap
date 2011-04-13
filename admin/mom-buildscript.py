ec209bb972bd31e2f843de49a98ccfb4a2caf059
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-04-13 12:11:00 +0000

    change buildname
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@39702 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- admin/mom-buildscript.py
+++ admin/mom-buildscript.py
@@ -12,7 +12,7 @@ from core.helpers.BoilerPlate import BuildProject
 from products.ProductBoilerPlate import ProductConfigurations
 from products.ProductPackager import ProductPackager
 
-build, project = BuildProject( name = 'KD SOAP', build = 'KD SOAP trunk', version = '1.1.0-trunk', url = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
+build, project = BuildProject( name = 'KD SOAP', version = '1.1.0-trunk', url = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
 
 configs = ProductConfigurations( project )
 
