2ef9ca77012ed903098a9992fd0ebd47415095dc
David Faure <david.faure@kdab.com>
2011-05-12 13:11:42 +0000

    Regenerated from products_buildsystem/generate-configure.sh  (but disabled the makeincludes.pl stuff for now)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@40010 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- kdsoap/FindKDSoap.cmake
+++ kdsoap/FindKDSoap.cmake
@@ -4,6 +4,7 @@
 # KDSoap_FOUND		- Set to TRUE if KDSoap was found.
 # KDSoap_LIBRARIES	- Path to KDSoap libraries.
 # KDSoap_INCLUDE_DIR	- Path to the KDSoap include directory.
+# KDSoap_CODEGENERATOR	- Path to the KDSoap code generator.
 #
 # This file was generated automatically.
 # Please edit generate-configure.sh rather than this file.
@@ -14,8 +15,11 @@ find_library(KDSoap_LIBRARIES
 	NAMES KDSoap kdsoap 
 	PATH_SUFFIXES bin)
 find_path(KDSoap_INCLUDE_DIR 
-	NAMES KDSoap KDSoapGlobal)
+	NAMES KDSoap KDSoapValue.h)
+find_program(KDSoap_CODEGENERATOR
+	NAMES kdwsdl2cpp
+	PATH_SUFFIXES bin)
 
-mark_as_advanced(KDSoap_LIBRARIES KDSoap_INCLUDE_DIR)
+mark_as_advanced(KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
 
-find_package_handle_standard_args(KDSoap DEFAULT_MSG KDSoap_LIBRARIES KDSoap_INCLUDE_DIR)
+find_package_handle_standard_args(KDSoap DEFAULT_MSG KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
--- kdsoap/configure.sh
+++ kdsoap/configure.sh
@@ -2,6 +2,8 @@
 # This file was generated automatically.
 # Please edit generate-configure.sh rather than this file.
 
+# configuration
+
 PRODUCT=KDSOAP
 Product=KDSoap
 product=kdsoap
@@ -10,30 +12,64 @@ ProductSpace="KD Soap"
 VERSION=1.1.0
 
 INSTALLATION_SUPPORTED=true
-STATIC_BUILD_SUPPORTED=false
+STATIC_BUILD_SUPPORTED=true
+HAS_DESIGNER_PLUGIN=false
 
-PACKSCRIPTS_DIR=../admin/packscripts
-
-hide_symbols=yes
-shared=yes
-debug=no
-release=yes
-prefix=
-unittests=no
-
-[ -d $PACKSCRIPTS_DIR ] && unittests=yes
+# function definitions
 
 function die {
     echo "$1" 1>&2
     exit 1
 }
 
+# find SRCDIR
+
+SRCDIR="$(dirname "$0")"
+case "$SRCDIR" in
+    /*) # absolute
+        ;;
+    *)  # relative - prepend $PWD
+        SRCDIR="$PWD/$SRCDIR"
+        ;;
+esac
+BUILDDIR="$PWD"
+PACKSCRIPTS_DIR="$SRCDIR/../admin/packscripts"
+
+# find Qt
+
+if [ -z "$QTDIR" ] ;  then
+    QTDIR="$(qmake -query QT_INSTALL_PREFIX)"
+    if [ $? -ne 0 ] ; then
+	QTDIR=
+    fi
+fi
+
+[ -z "$QTDIR" ] && die "You need QTDIR defined, or qmake in the PATH"
+
+# config variables
+
+hide_symbols=yes
+shared=yes
+debug=no
+release=yes
+prefix=
+plugins_prefix=
+unittests=no
+
+
+if [ -d "$PACKSCRIPTS_DIR" ] ; then
+    unittests=yes
+
+fi
+
+# check licence
+
 function check_license {
 
-    [ -d $PACKSCRIPTS_DIR ] && return 0 
-    [ -f .license.accepted ] && return 0
+    [ -d "$PACKSCRIPTS_DIR" ] && return 0 
+    [ -f "$SRCDIR/.license.accepted" ] && return 0
 
-    if [ -f LICENSE.GPL.txt -a -f LICENSE.US.txt -a -f LICENSE.txt ] ; then
+    if [ -f "$SRCDIR/LICENSE.GPL.txt" -a -f "$SRCDIR/LICENSE.US.txt" -a -f "$SRCDIR/LICENSE.txt" ] ; then
         echo
         echo "Please choose your license."
         echo
@@ -45,13 +81,13 @@ function check_license {
         echo -n "Select: "
         read license
 
-    elif [ -f LICENSE.GPL.txt ] ; then
+    elif [ -f "$SRCDIR/LICENSE.GPL.txt" ] ; then
         license="1"
 
-    elif [ -f LICENSE.US.txt ] ; then
+    elif [ -f "$SRCDIR/LICENSE.US.txt" ] ; then
         license="2"
 
-    elif [ -f LICENSE.txt ] ; then
+    elif [ -f "$SRCDIR/LICENSE.txt" ] ; then
         license="3"
     else
         die "Couldn't find license file, aborting"
@@ -87,14 +123,16 @@ EOF
         read answer
 
 	[ "$answer" = "no" ]  && return 1
-	[ "$answer" = "yes" ] && touch .license.accepted && return 0
-	[ "$answer" = "?" ]   && more $license_file
+	[ "$answer" = "yes" ] && touch "$SRCDIR/.license.accepted" && return 0
+	[ "$answer" = "?" ]   && more "$SRCDIR/$license_file"
     done
 }
 
 if ! check_license ; then
     die "You are not licensed to use this software."
 fi
+
+# options parsing
 
 function usage {
     [ -z "$1" ] || echo "$0: unknown option \"$1\"" 1>&2
@@ -108,6 +146,12 @@ if [ "$INSTALLATION_SUPPORTED" = "true" ]; then
   -prefix <path>
       install $ProductSpace into <path>
 EOF
+if [ "$HAS_DESIGNER_PLUGIN" = "true" ]; then
+    cat <<EOF 1>&2
+  -plugins-prefix <path>
+      install $Product\'s designer plugin into <path>/designer
+EOF
+fi  
 fi  
 cat <<EOF 1>&2
 
@@ -126,27 +170,31 @@ cat <<EOF 1>&2
   -[no-]hide-symbols (Unix only)
       reduce the number of exported symbols
 
-  -[no-]unittests
-      enable/disable compiled-in unittests
-
   -spec <mkspec>
       compile $ProductSpace for specific Qt-supported target <mkspec>
+
+  -unittests / -no-unittests
+      enable/disable compiled-in unittests
+
 
 EOF
     exit 1
 }
 
-if [ -z "$QTDIR" ] ;  then
-    QTDIR="$(qmake -query QT_INSTALL_PREFIX)"
-    if [ $? -ne 0 ] ; then
-	QTDIR=
-    fi
-fi
-
-[ -z "$QTDIR" ] && die "You need QTDIR defined, or qmake in the PATH"
-
 while [ $# -ne 0 ] ; do
     case "$1" in
+        -plugins-prefix)
+            if [ "$INSTALLATION_SUPPORTED" != "true" -o "$HAS_DESIGNER_PLUGIN" != "true" ]; then
+              echo "Installation not supported, -plugins-prefix option ignored" 2>&1
+#             usage
+            fi
+            shift
+            if [ $# -eq 0 ] ; then
+                    echo "-plugins-prefix needs an argument" 2>&1
+                    usage
+            fi
+            plugins_prefix="$1"
+            ;;
 	-prefix)
             if [ "$INSTALLATION_SUPPORTED" != "true" ]; then
 	      echo "Installation not supported, -prefix option ignored" 2>&1
@@ -173,12 +221,6 @@ while [ $# -ne 0 ] ; do
             fi
             VERSION="$1"
             ;;
-	-no-unittests)
-	    unittests=no
-	    ;;
-	-unittests)
-	    unittests=yes
-	    ;;
         -shared)
             shared=yes
             ;;
@@ -205,6 +247,13 @@ while [ $# -ne 0 ] ; do
             fi
             SPEC="-spec $1"
             ;;
+        -no-unittests)
+            unittests=no
+            ;;
+        -unittests)
+            unittests=yes
+            ;;
+
         *)
             usage "$1"
             ;;
@@ -212,28 +261,52 @@ while [ $# -ne 0 ] ; do
     shift
 done
 
-find . -name debug -o -name release -o -name Makefile\* | xargs rm -rf
+# apply defaults to unset options
 
-if [ -f src/src.pro ] ; then
-    rm -rf lib bin
-fi
+readonly default_prefix=/usr/local/KDAB/$Product-$VERSION
+readonly default_plugins_prefix="$("$QTDIR/bin/qmake" -query QT_INSTALL_PLUGINS)"
 
-#if [ -d $PACKSCRIPTS_DIR ] ; then
-#    echo
-#    echo -n "Creating include directory..."
-#    perl $PACKSCRIPTS_DIR/makeincludes.pl > makeincludes.log 2>&1 || die "Failed to run $PACKSCRIPTS_DIR/makeincludes.pl"
-#    rm makeincludes.log
-#    echo done
-#    echo
-#fi
-
-default_prefix=/usr/local/KDAB/$Product-$VERSION
 if [ -z "$prefix" ] ; then
     prefix="$default_prefix"
 fi
+if [ -z "$plugins_prefix" ] ; then
+    if [ -w "$default_plugins_prefix" ] ; then
+        plugins_prefix="$default_plugins_prefix"
+    else
+        plugins_prefix="$prefix/plugins"
+    fi
+fi
+
+
+# clean previous build
+
+find . -name debug -o -name release -o -name Makefile\* | xargs rm -rf
+
+if [ -f "$SRCDIR/src/src.pro" ] ; then
+    rm -rf lib bin
+fi
+
+# call makeincludes.pl
+
+if false ; then   ### TODO if [ -d "$PACKSCRIPTS_DIR" ] ; then
+    echo
+    echo -n "Creating include directory..."
+    (
+        cd "$SRCDIR" || die "Failed to cd into SRCDIR ($SRCDIR)"
+        perl "$PACKSCRIPTS_DIR/makeincludes.pl" > makeincludes.log 2>&1 || die "Failed to run $PACKSCRIPTS_DIR/makeincludes.pl"
+        rm makeincludes.log
+    ) || exit 1
+    echo done
+    echo
+fi
+
+# write .qmake.cache
 
 echo -n > ".qmake.cache"
 (
+    echo "${PRODUCT}_SOURCE_DIR=\"$SRCDIR\""
+    echo "${PRODUCT}_BUILD_DIR=\"$BUILDDIR\""
+
     echo "VERSION=$VERSION"
     echo "CONFIG += ${product}_target"
 
@@ -252,6 +325,7 @@ echo -n > ".qmake.cache"
     [ "$hide_symbols" = "yes" ] &&  echo "CONFIG += hide_symbols"
     [ "$unittests" = "yes" ] && echo "CONFIG += unittests"
 
+
     if [ "$shared" = "yes" ]; then
       echo "CONFIG -= static"
       echo "CONFIG -= staticlib"
@@ -269,11 +343,38 @@ echo -n > ".qmake.cache"
         #echo "QTDIR must point to an installation that has private headers installed."
         #echo "Some features will not be available."    
     fi
-[ "$INSTALLATION_SUPPORTED" = "true" ] && echo "${PRODUCT}_INSTALL_PREFIX = $prefix"
+    if [ "$INSTALLATION_SUPPORTED" = "true" ] ; then
+        echo "${PRODUCT}_INSTALL_PREFIX = $prefix"
+        if [ "$HAS_DESIGNER_PLUGIN" = "true" ] ; then
+            echo "${PRODUCT}_PLUGINS_PREFIX = $plugins_prefix"
+        fi
+    fi
 
 ) >> ".qmake.cache"
 
+# link .prf files to builddir
+
+if [ ! -f ${product}.pro ] ; then # ie. srcdir != builddir
+    if [ -d "$SRCDIR/features" ] ; then
+        [ -d "$BUILDDIR/features" ] || mkdir "$BUILDDIR/features" || die "Could not create directory $BUILDDIR/features"
+        for P in "$SRCDIR/features"/*.prf ; do
+            name=$(basename "$P")
+            echo "include( \"\$\$${PRODUCT}_SOURCE_DIR/features/$name\" )" > "$BUILDDIR/features/$name" || die "Could not create forward .prf file $BUILDDIR/features/$name"
+        done
+    fi
+fi
+
+# call qmake
+
+# Make a copy so that each run of qmake on $product.pro starts clean
+cp -f .qmake.cache .confqmake.cache
+
+"$QTDIR/bin/qmake" ${SPEC} -recursive  "$SRCDIR/$product.pro" "${PRODUCT}_BASE=`pwd`" || die "qmake failed"
+
+# display configuration summary
+
 cat <<EOF 1>&2
+
 $ProductSpace v$VERSION configuration:
 EOF
 
@@ -282,6 +383,12 @@ cat <<EOF 1>&2
   Install Prefix.............: $prefix
     (default: $default_prefix)
 EOF
+if [ "$HAS_DESIGNER_PLUGIN" = "true" ]; then
+cat <<EOF 1>&2
+  Install Plugins Prefix.....: $plugins_prefix
+    (default: $default_plugins_prefix)
+EOF
+fi
 fi
 
 cat <<EOF 1>&2
@@ -299,19 +406,20 @@ if [ "$SPEC" != "" ]; then
 EOF
 fi
 cat <<EOF 1>&2
-  Compiled-In Unit Tests.....: $unittests (default: no)
   Restricted symbol export
     (shared build only)......: $hide_symbols (default: yes)
+  Compiled-In Unit Tests.....: $unittests (default: no)
 
 EOF
 
-# Make a copy so that each run of qmake on $product.pro starts clean
-cp -f .qmake.cache .confqmake.cache
-
-$QTDIR/bin/qmake ${SPEC} $product.pro -recursive "${PRODUCT}_BASE=`pwd`" || die "qmake failed"
-
+echo
 if [ "$INSTALLATION_SUPPORTED" = "true" ]; then
   echo "Ok, now run make, then make install to install into $prefix"
+  if [ "$HAS_DESIGNER_PLUGIN" = "true" ] ; then
+  if [ "$plugins_prefix" != "$default_plugins_prefix" ] ; then
+      echo "Please add $plugins_prefix to Qt's list of plugin paths."
+  fi
+  fi
 else
   echo "Ok, now run make to build the framework."
 fi 
