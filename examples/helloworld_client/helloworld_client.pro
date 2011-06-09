4c9590c19c3dc3f38dc552c25fd8535f1a81d4f4
Frank Osterfeld <frank.osterfeld@kdab.com>
2011-06-09 07:09:39 +0000

    add a GUI for the helloworld_client. Simplify the wsdl.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@40367 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/helloworld_client/helloworld_client.pro
+++ examples/helloworld_client/helloworld_client.pro
@@ -3,9 +3,11 @@ KDSOAP_PATH = $$PWD/../..
 include( $$KDSOAP_PATH/examples/examples.pri )
 
 SOURCES = main.cpp
+HEADERS += helloworld_client.h
 
 KDWSDL = helloworld.wsdl
 
-#CONFIG -= console
-#QT += gui
-#macx:CONFIG += app_bundle
+CONFIG -= console
+QT += gui
+macx:CONFIG += app_bundle
+
--- examples/helloworld_server/helloworld_server.h
+++ examples/helloworld_server/helloworld_server.h
@@ -8,27 +8,10 @@ class ServerObject : public Hello_ServiceServerBase
     Q_OBJECT
     Q_INTERFACES(KDSoapServerObjectInterface)
 public:
-    ServerObject() : Hello_ServiceServerBase() {
-    }
+    ServerObject();
+    ~ServerObject();
 
-    ~ServerObject() {
-    }
-
-    QString sayHello(const QString& msg)
-    {
-    if (msg.isEmpty()) {
-            setFault(QLatin1String("Client.Data"), QLatin1String("Empty message"),
-                     QLatin1String("ServerObject"), tr("You must say something."));
-            return QString();
-        }
-        return tr("I'm helloworld_server and you said: %1").arg(msg);
-    }
-
-    QString sayHelloAndGetAnError(const QString&) {
-        setFault(QLatin1String("Client.Data"), QLatin1String("Some error"),
-                 QLatin1String("ServerObject"), tr("This method always returns an error."));
-        return QString();
-    }
+    QString sayHello(const QString& msg);
 };
 
 #endif // HELLOWORLD_SERVER_H
