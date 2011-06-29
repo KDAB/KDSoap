d9d2237843288bdabdd7067105ee08c5ab82aadc
Bjoern Breitmeyer <bjoern.breitmeyer@kdab.com>
2011-06-29 07:35:23 +0000

    added the license information to the files
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.1.0-release@40657 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- kdsoap/src/KDSoapClient/KDSoapPendingCallWatcher.cpp
+++ kdsoap/src/KDSoapClient/KDSoapPendingCallWatcher.cpp
@@ -1,3 +1,24 @@
+/****************************************************************************
+** Copyright (C) 2001-2011 Klaralvdalens Datakonsult AB.  All rights reserved.
+**
+** This file is part of the KD Soap library.
+**
+** Licensees holding valid commercial KD Soap licenses may use this file in
+** accordance with the KD Soap Commercial License Agreement provided with
+** the Software.
+**
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.GPL.txt included.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Contact info@kdab.com if any conditions of this licensing are not
+** clear to you.
+**
+**********************************************************************/
 #include "KDSoapPendingCallWatcher.h"
 #include "KDSoapPendingCallWatcher_p.h"
 #include "KDSoapPendingCall_p.h"
