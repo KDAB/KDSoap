56a501dc8284f47c46bdd25a39bf02bbc75e8843
Bjoern Breitmeyer <bjoern.breitmeyer@kdab.com>
2011-06-29 08:04:32 +0000

    corrected version
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.1.0-release@40658 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- admin/mom-buildscript.py
+++ admin/mom-buildscript.py
@@ -11,7 +11,7 @@
 from core.helpers.BoilerPlate import BuildProject
 from products.ProductBoilerPlate import ProductConfigurations
 
-build, project = BuildProject( name = 'KD SOAP', version = '1.1.0-trunk', url = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
+build, project = BuildProject( name = 'KD SOAP', version = '1.1.0', url = 'svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap' )
 #build, project = BuildProject( name = 'KD SOAP', version = '1.1.0-trunk', url = 'git:/home/bjoern/projects/products/kdsoap_trunk' )
 configs = ProductConfigurations( project )
 configs.addForwardHeaderGenerator( "KDSoap", ["KDSoapClient","KDSoapServer"], "INSTALL_PREFIX", True )
