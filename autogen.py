56b60ebbab6e4050ee4397d14c9594df599cc3ec
Mike McQuaid <mike.mcquaid@kdab.com>
2011-09-27 15:57:28 +0000

    Use new minimal autogen.py script
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/buildsystem/autotools@42159 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- autogen.py
+++ autogen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from autogen.autogen import autogen
 
-project = "KDChart"
-version = "2.5.0"
-subprojects = ["KDChart", "KDGantt"]
+project = "KDReports"
+version = "1.3.0"
+subprojects = ["KDReports"]
 prefixed = True
 
 autogen(project, version, subprojects, prefixed)
