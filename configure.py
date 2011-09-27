7b842827e7949f34ac95a93b590e801f8b4520b9
Mike McQuaid <mike.mcquaid@kdab.com>
2011-09-27 15:54:40 +0000

    Split autogen.py into directories.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/buildsystem/autotools@42157 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ configure.py
@@ -0,0 +1,40 @@
+import os.path
+
+class ConfigureScriptGenerator():
+	def __init__( self, project, path, version, install = True, static = True ):
+		self.__project = project
+		self.__path = path
+		self.__version = version
+		autogen_dir = os.path.dirname( __file__ )
+		self.__winTemplate = os.path.abspath( autogen_dir + "/configure.bat.in" )
+		self.__unixTemplate = os.path.abspath( autogen_dir + "/configure.sh.in" )
+
+	def run( self ):
+		self.__generateFile( self.__unixTemplate, os.path.abspath( self.__path + "/configure.sh" ), "unix" )
+		self.__generateFile( self.__winTemplate, os.path.abspath( self.__path + "/configure.bat" ), "win32" )
+
+	def __replaceValues( self, value ):
+		mixedname = self.__project
+		mixedname = mixedname.replace( "KD", "KD " )
+		value = value.replace( "@VERSION@", self.__version )
+		value = value.replace( "@PRODUCT_UPPERCASE@", self.__project.upper() )
+		value = value.replace( "@PRODUCT_LOWERCASE@", self.__project.lower() )
+		value = value.replace( "@PRODUCT_MIXEDCASE@", self.__project )
+		value = value.replace( "@PRODUCT_MIXEDCASE_SPACED@", mixedname )
+		return value
+
+	def __generateFile( self, templateFile, outputFile, platformString ):
+		if platformString == "win32":
+			lineSep = "\r\n"
+		else:
+			lineSep = "\n"
+
+		with open( outputFile, "wb" ) as fOutput:
+			with open(os.path.dirname(__file__) + '/' + os.path.basename(outputFile) + '.in') as configureFile:
+				configure = configureFile.read().splitlines()
+			for line in ( configure ):
+				fOutput.write( self.__replaceValues( line.rstrip() ) + lineSep )
+
+		# make file executable for Unix
+		if platformString != "win32":
+			os.chmod( outputFile, 0755 )
---
+++ cpack.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import os.path
+
+class CPackGenerateConfiguration():
+	def __init__( self, projectName, version, directory, revision,
+	              licenseFile = "LICENSE.txt" ):
+		self._projectName = projectName
+		self._directory = directory
+		self._revision = revision 
+		self._licenseFile = licenseFile
+		versionList = version.split( "." )
+		assert( isinstance( versionList, list ) and len( versionList ) == 3 )
+		self._versionList = versionList
+		self._generators = { 'WINDOWS': 'NSIS', 'APPLE': 'ZIP', 'ELSE': 'TBZ2' }
+
+	def fixCMakeWindowsPaths( self, path ):
+		return path.replace( '\\', '\\\\' )
+
+	def _formattedConfiguration( self ):
+		with open(os.path.dirname(__file__) + '/cpack.cmake.in') as configFile:
+			config = configFile.read() 
+
+		# Can't do this with str.format because of CMake's variable escaping conflicting
+		# with Python's format escaping
+		packageName = self._projectName
+		packageNameSimplified = packageName.lower().replace( ' ', '_' )
+		config = config.replace( "@CPACK_PACKAGE_NAME@", packageName, 1 )
+		config = config.replace( "@CPACK_PACKAGE_NAME_SIMPLIFIED@", packageNameSimplified, 1 )
+
+		versionList = self._versionList
+		config = config.replace( "@CPACK_PACKAGE_VERSION_MAJOR@", versionList[0] or 1, 1 )
+		config = config.replace( "@CPACK_PACKAGE_VERSION_MINOR@", versionList[1] or 0, 1 )
+		patchVersion = versionList[2] or 0
+		patchVersion += '-r' + self._revision
+		config = config.replace( "@CPACK_PACKAGE_VERSION_PATCH@", patchVersion, 1 )
+		installDirectory = self.fixCMakeWindowsPaths( self._directory )
+		config = config.replace( "@CPACK_INSTALL_DIRECTORY@", installDirectory, 1 )
+
+		licenseFile = self._licenseFile
+
+		if not licenseFile:
+			licenseFile = os.path.join( self._directory, "CPackGeneratedLicense.txt" )
+			with open( licenseFile, 'w' ) as license:
+				license.write( '{0} - Copyright {1}, All Rights Reserved.'.format( packageName, datetime.now().year ) )
+		else:
+			licenseFile = os.path.abspath( licenseFile ) # NSIS apparently requires an absolute path to find the license file
+		licenseFile = self.fixCMakeWindowsPaths( licenseFile )
+
+		config = config.replace( "@CPACK_RESOURCE_FILE_LICENSE@", licenseFile )
+
+		for platform in ( 'WINDOWS', 'APPLE', 'ELSE' ):
+			generator = self._generators[ platform ]
+			config = config.replace( "@CPACK_GENERATOR_%s@" % platform, generator )
+
+		return config
+
+	def run( self ):
+		config = os.path.join( self._directory, 'CPackConfig.cmake' )
+		with open( config, 'w' ) as configFile:
+			configFile.write( self._formattedConfiguration() )
