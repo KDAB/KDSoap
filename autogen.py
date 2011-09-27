49ce47ac511bb75241f40cccd78e915bfa183ef8
Mike McQuaid <mike.mcquaid@kdab.com>
2011-09-27 16:01:32 +0000

    Fix overwritten autogen.py
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/buildsystem/autotools@42160 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- autogen.py
+++ autogen.py
@@ -1,9 +1,56 @@
-#!/usr/bin/env python
-from autogen.autogen import autogen
+import os, sys, subprocess
 
-project = "KDReports"
-version = "1.3.0"
-subprojects = ["KDReports"]
-prefixed = True
+from cpack import CPackGenerateConfiguration 
+from configure import ConfigureScriptGenerator
+from header import ForwardHeaderGenerator
 
-autogen(project, version, subprojects, prefixed)
+def autogen(project, version, subprojects, prefixed):
+	sourceDirectory = os.path.abspath( os.path.dirname( os.path.dirname( __file__ ) ) )
+	buildDirectory = os.getcwd()
+
+	print( "-- Using source directory: {0}".format( sourceDirectory ) )
+
+	# check repository URL
+	p = subprocess.Popen( ["svn", "info"], cwd = sourceDirectory, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+	( stdout, stderr ) = p.communicate()
+	if p.returncode != 0:
+		p = subprocess.Popen( ["git", "svn", "info"], cwd = sourceDirectory, stdout = subprocess.PIPE, stderr = subcprocess.PIPE )
+		( stdout, stderr ) = p.communicate()
+	if p.returncode != 0:
+		print_stderr( "Error: Not a SVN repository: {0}".format( sourceDirectory ) )
+		sys.exit( 1 )
+
+	repositoryUrl = stdout.splitlines()[1].split( ':', 1 )[1]
+	repositoryRevision = stdout.splitlines()[4].split( ':', 1 )[1].strip()
+
+	cpackConfigurationGenerator = CPackGenerateConfiguration( project, version, buildDirectory, repositoryRevision)
+	cpackConfigurationGenerator.run()
+
+	configureScriptGenerator = ConfigureScriptGenerator( project, buildDirectory, version )
+	configureScriptGenerator.run()
+
+	includePath = os.path.join( sourceDirectory, "include" )
+	srcPath = os.path.join( sourceDirectory, "src" )
+
+	if subprojects:
+		forwardHeaderGenerator = ForwardHeaderGenerator( 
+			copy = True, path = sourceDirectory, includepath = includePath, srcpath = srcPath,
+			project = project, subprojects = subprojects, prefix = "$$INSTALL_PREFIX", prefixed = prefixed 
+		)
+		forwardHeaderGenerator.run()
+
+	print( "-- Auto-generation done." )
+
+	with file( ".license.accepted", 'a' ):
+		os.utime( ".license.accepted", None )
+	print( "-- License marked as accepted." )
+
+	print( "-- Wrote build files to: {0}".format( buildDirectory ) )
+	print( "-- Now running configure script." )
+	print( "" )
+	sys.stdout.flush()
+
+	if sys.platform == 'win32':
+		os.execvp( './configure.bat', ['configure.bat'] + sys.argv[1:] )
+	else:
+		os.execvp( './configure.sh', ['configure.sh'] + sys.argv[1:] )
