d8388d8f5171c4e6905c1aadc42e7b60549a1511
David Faure <david.faure@kdab.com>
2011-11-14 15:24:37 +0000

    Fix example program after change of web services (other examples and tests were changed, but this one got forgotten)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@42907 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/holidays_sync/holidays.cpp
+++ examples/holidays_sync/holidays.cpp
@@ -10,18 +10,21 @@ int main(int argc, char **argv)
 
     const int year = 2009;
 
-    const QString endPoint = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/USHolidayDates.asmx");
+    const QString endPoint = QLatin1String("http://www.holidaywebservice.com/Holidays/US/Dates/USHolidayDates.asmx");
     const QString messageNamespace = QLatin1String("http://www.27seconds.com/Holidays/US/Dates/");
     KDSoapClientInterface client(endPoint, messageNamespace);
 
     KDSoapMessage message;
+    message.setQualified(true);
     message.addArgument(QLatin1String("year"), year);
 
     qDebug("Looking up the date of Valentine's Day in %i...", year);
 
     KDSoapMessage response = client.call(QLatin1String("GetValentinesDay"), message);
-
-    qDebug("%s", qPrintable(response.arguments()[0].value().toString()));
+    if (response.isFault())
+        printf("%s\n", qPrintable(response.faultAsString()));
+    else
+        printf("%s\n", qPrintable(response.arguments()[0].value().toString()));
 
     return 0;
 }
