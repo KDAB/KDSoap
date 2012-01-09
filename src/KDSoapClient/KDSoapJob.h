f9b7be1e7210c64e8c275a682376ada62872b00b
Frank Osterfeld <frank.osterfeld@kdab.com>
2012-01-09 10:39:53 +0000

    Add doxygen for KDSoapJob
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@44144 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDSoapClient/KDSoapJob.h
+++ src/KDSoapClient/KDSoapJob.h
@@ -28,24 +28,95 @@
 
 class KDSoapMessage;
 
+/**
+ * \brief KDSoapJob provides a job-based interface to handle asynchronous KD Soap calls.
+ * For each SOAP operation, kdwsdl2cpp generates a corresponding KDSoapJob subclass.
+ * This makes it easier to manage multiple callers in the client application code calling the same functions, preventing
+ * callers processing results for calls made from another caller instead of their own calls.
+ *
+ * For example, a call to getMothersDay (from the holidays example) using the conventional asynchronous API would look like this:
+ * \code
+ *  connect(client, SIGNAL(getMothersDayDone(TNS__GetMothersDayResponse)), this, SLOT(getMothersDayDone(TNS__GetMothersDayResponse)));
+ *  connect(client, SIGNAL(getMothersDayError(KDSoapMessage)), this, SLOT(getMothersDayError(KDSoapMessage)));
+ *  TNS__GetMothersDay params;
+ *  params.setYear(2012);
+ *  client->asyncGetMothersDay(params);
+ * \endcode
+ *
+ * In this example, all responses to getMothersDay made \c client will be received by the slots connected above.
+ * Using the job-based API, it becomes:
+ *
+ * \code
+ *  GetMothersDayJob* job = new GetMothersDayJob(client);
+ *  connect(job, SIGNAL(finished(KDJob*)), this, SLOT(getMothersDayDone(KDSoapJob*)));
+ *  TNS__GetMothersDay params;
+ *  params.setYear(2012);
+ *  job->setParameters(params);
+ *  job->start();
+ * \endcode
+ *
+ * Now only the result of this specific getMothersDay call will be received by the slot.
+ *
+ * \since 1.2
+ */
 class KDSOAP_EXPORT KDSoapJob : public QObject {
     Q_OBJECT
 public:
-    explicit KDSoapJob(QObject *parent=0);
+    /**
+     * Constructs a KD Soap job.
+     *
+     * \param parent optional parent object
+     */
+    explicit KDSoapJob(QObject * parent=0);
+
+    /**
+     * Destructor.
+     */
     ~KDSoapJob();
 
+    /**
+     * Returns whether the reply message (see reply()) represents a fault.
+     */
     bool isFault() const;
+
+    /**
+     * A human-readable error string describing the fault if the reply message is a fault, an empty string otherwise.
+     */
     QString faultAsString() const;
+
+    /**
+     * Returns the reply message received from the SOAP server once the request was completed.
+     * Only valid once the request is completed and finished() was emitted.
+     */
     KDSoapMessage reply() const;
 
+    /**
+     * Starts the job. The job will emit finished() once done.
+     */
     void start();
 
 Q_SIGNALS:
-    void finished(KDSoapJob *);
+    /**
+     * emitted when the job is completed, i.e. the reply for the job's request
+     * was received. To read the result, call reply() in the connected slot.
+     * Do not delete the job, the job will auto-delete itself.
+     *
+     * \param job The job instance that emitted the signal
+     */
+    void finished(KDSoapJob * job);
 
 protected:
+    /**
+     * \internal
+     * Reimplemented in kdwsdl2cpp-generated classes to start request asynchronously.
+     */
     Q_INVOKABLE virtual void doStart() = 0;
-    void emitFinished(const KDSoapMessage &reply);
+
+    /**
+     * \internal
+     * Sets reply, emits finished() signal and manages deletion of job
+     */
+    void emitFinished(const KDSoapMessage & reply);
 
 private:
     class Private;
