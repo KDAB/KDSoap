a4a1afc043f73ad7f74da550b9eff7a8559cff17
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2012-01-23 23:46:56 +0000

    Add genignore.py, which generates a list of files to be excluded from packaging, and the result of running it, CPackIgnores.txt. Now the generated source packages on Linux and Mac are free of binaries.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/branches/kdsoap-1.2.0-release@44530 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ genignore.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os, stat
+
+# This script, when run in KD Soap's source and build directory containing the results of a full
+# build, writes a list of files to be excluded from packaging to CPackIgnores.txt.
+# autogen reads that list and passes it to CPack.
+
+# You should re-generate CPackIgnores.txt before every release and also check that no new binary
+# slips through the cracks using
+# find . -type f -executable
+
+sourceDirectory = os.path.abspath( os.path.dirname( os.path.dirname( __file__ ) ) )
+
+def ignoredFiles():
+	ret = []
+	def findExecutables( top ):
+		for f in os.listdir( top ):
+			pathname = os.path.join( top, f )
+			if stat.S_ISDIR( os.stat( pathname ).st_mode ):
+				findExecutables( pathname )
+			elif os.access( pathname, os.X_OK ):
+				# The file is executable for us
+				ret.append( pathname )
+
+	# With one exception, the executables in those paths are binaries and we're making a SOURCE package.
+	for path in [ 'unittests' ]:
+		findExecutables( os.path.join( sourceDirectory, path ) )
+	# The exception!
+	def isGoodExclude(s): return s != os.path.join( sourceDirectory, 'unittests/runTest.bat' )
+	return filter( isGoodExclude, ret )
+
+f = open( 'CPackIgnores.txt', 'w')
+for ign in ignoredFiles():
+	# write paths relative to the source dir, one per line
+	f.write( ign[ len( sourceDirectory ) : ] + '\n' )
