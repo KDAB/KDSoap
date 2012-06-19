cb727e81fc931ac8aaf756ac3f2072c42507c55f
David Faure <david.faure@kdab.com>
2012-06-19 22:16:17 +0000

    Relicense to LGPL
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdsoap/trunk@46749 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDSoapClient/KDSoapNamespaceManager.cpp
+++ src/KDSoapClient/KDSoapNamespaceManager.cpp
@@ -9,8 +9,8 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 and version 3 as published by the
-** Free Software Foundation and appearing in the file LICENSE.GPL.txt included.
+** GNU Lesser General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.LGPL.txt included.
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
