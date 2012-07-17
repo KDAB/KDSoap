47a4c22d36e99f19c280048f457eced7cd8acede
Montel Laurent <laurent@kdab.com>
2012-07-17 15:05:49 +0200

    Remove not necessary macro

--- KDSoapMacros.cmake
+++ KDSoapMacros.cmake
@@ -1,5 +1,9 @@
 macro( KDSOAP_GENERATE_WSDL _sources )
   set(KDWSDL2CPP kdwsdl2cpp )
+  set(_KSWSDL2CPP_OPTION)
+  if(KSWSDL2CPP_OPTION)
+     set(_KSWSDL2CPP_OPTION ${KSWSDL2CPP_OPTION})
+  endif(KSWSDL2CPP_OPTION)
   foreach (_source_FILE ${ARGN})
     get_filename_component(_tmp_FILE ${_source_FILE} ABSOLUTE)
     get_filename_component(_basename ${_tmp_FILE} NAME_WE)
@@ -8,13 +12,13 @@ macro( KDSOAP_GENERATE_WSDL _sources )
     
     add_custom_command(OUTPUT ${_header_wsdl_FILE} 
        COMMAND ${KDWSDL2CPP}
-       ARGS ${_tmp_FILE} -o ${_header_wsdl_FILE}
+       ARGS ${_KSWSDL2CPP_OPTION} ${_tmp_FILE} -o ${_header_wsdl_FILE}
        MAIN_DEPENDENCY ${_tmp_FILE}
        DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
 
     add_custom_command(OUTPUT ${_source_wsdl_FILE} 
        COMMAND ${KDWSDL2CPP}
-       ARGS -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
+       ARGS ${_KSWSDL2CPP_OPTION} -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
        MAIN_DEPENDENCY ${_tmp_FILE} ${_header_wsdl_FILE}
        DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
 
@@ -22,29 +26,4 @@ macro( KDSOAP_GENERATE_WSDL _sources )
     list(APPEND ${_sources} ${_header_wsdl_FILE} ${_source_wsdl_FILE} ${_sources_MOCS})
   endforeach (_source_FILE)
 endmacro(KDSOAP_GENERATE_WSDL _sources )
-
-macro( KDSOAP_GENERATE_SERVER_WSDL _sources )
-  set(KDWSDL2CPP kdwsdl2cpp )
-  foreach (_source_FILE ${ARGN})
-    get_filename_component(_tmp_FILE ${_source_FILE} ABSOLUTE)
-    get_filename_component(_basename ${_tmp_FILE} NAME_WE)
-    set(_header_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.h)
-    set(_source_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.cpp)
-
-    add_custom_command(OUTPUT ${_header_wsdl_FILE} 
-       COMMAND ${KDWSDL2CPP}
-       ARGS -server ${_tmp_FILE} -o ${_header_wsdl_FILE}
-       MAIN_DEPENDENCY ${_tmp_FILE}
-       DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
-
-    add_custom_command(OUTPUT ${_source_wsdl_FILE} 
-       COMMAND ${KDWSDL2CPP}
-       ARGS -server -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
-       MAIN_DEPENDENCY ${_tmp_FILE} ${_header_wsdl_FILE}
-       DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
-
-    qt4_wrap_cpp(_sources_MOCS ${_header_wsdl_FILE} )
-    list(APPEND ${_sources} ${_header_wsdl_FILE} ${_source_wsdl_FILE} ${_sources_MOCS})
-  endforeach (_source_FILE)
-endmacro(KDSOAP_GENERATE_SERVER_WSDL _sources )
 
