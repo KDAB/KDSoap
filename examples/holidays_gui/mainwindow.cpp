c2525b9e97dede835d86a742dd92225e57891272
David Faure <david.faure@kdab.com>
2012-12-18 18:28:39 +0100

    Fix compilation with Qt5 (fromAscii is gone)

--- examples/holidays_gui/mainwindow.cpp
+++ examples/holidays_gui/mainwindow.cpp
@@ -17,7 +17,7 @@ MainWindow::MainWindow( QWidget *parent ) : QWidget( parent )
   mLblResult = new QLabel(tr("Result: "), this);
   mLblAnim = new QLabel(this);
   
-  mMovAnim = new QMovie(QString::fromAscii(":/animations/anim.mng"));
+  mMovAnim = new QMovie(QString::fromLatin1(":/animations/anim.mng"));
   
   QVBoxLayout *centralLayout = new QVBoxLayout(this);
   QHBoxLayout *btnsLayout = new QHBoxLayout();
