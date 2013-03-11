06d0312f6e925fe71d0bd61e7d9545f19acd4289
Stephen Kelly <stephen.kelly@kdab.com>
2013-03-11 14:21:47 +0100

    Trim trailing whitespace from the macros file.

--- KDSoapMacros.cmake
+++ KDSoapMacros.cmake
@@ -9,19 +9,19 @@ macro( KDSOAP_GENERATE_WSDL _sources )
     get_filename_component(_basename ${_tmp_FILE} NAME_WE)
     set(_header_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.h)
     set(_source_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.cpp)
-    add_custom_command(OUTPUT ${_header_wsdl_FILE} 
+    add_custom_command(OUTPUT ${_header_wsdl_FILE}
        COMMAND ${KDWSDL2CPP}
        ARGS ${_KSWSDL2CPP_OPTION} ${_tmp_FILE} -o ${_header_wsdl_FILE}
        MAIN_DEPENDENCY ${_tmp_FILE}
        DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
 
-    add_custom_command(OUTPUT ${_source_wsdl_FILE} 
+    add_custom_command(OUTPUT ${_source_wsdl_FILE}
        COMMAND ${KDWSDL2CPP}
        ARGS ${_KSWSDL2CPP_OPTION} -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
        MAIN_DEPENDENCY ${_tmp_FILE} ${_header_wsdl_FILE}
        DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
 
-    qt4_wrap_cpp(_sources_MOCS ${_header_wsdl_FILE} ) 
+    qt4_wrap_cpp(_sources_MOCS ${_header_wsdl_FILE} )
     list(APPEND ${_sources} ${_header_wsdl_FILE} ${_source_wsdl_FILE} ${_sources_MOCS})
   endforeach ()
 endmacro()
