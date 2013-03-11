985cfccbcd181732042873c4902152703f6a4c6a
Chris Harris <chris.harris@kitware.com>
2013-03-11 12:37:35 -0400

    Use GNUInstallDirs to determine install dirs

--- cmake/InstallLocation.cmake
+++ cmake/InstallLocation.cmake
@@ -4,15 +4,19 @@
 #
 # These paths attempt to adhere to the FHS, and are similar to those provided
 # by autotools and used in many Linux distributions.
+
+# Use GNU install directories
+include(GNUInstallDirs)
+
 if(NOT INSTALL_RUNTIME_DIR)
-  set(INSTALL_RUNTIME_DIR "bin")
+  set(INSTALL_RUNTIME_DIR ${CMAKE_INSTALL_BINDIR})
 endif()
 if(NOT INSTALL_LIBRARY_DIR)
-  set(INSTALL_LIBRARY_DIR "lib")
+  set(INSTALL_LIBRARY_DIR  ${CMAKE_INSTALL_LIBDIR})
 endif()
 if(NOT INSTALL_ARCHIVE_DIR)
-  set(INSTALL_ARCHIVE_DIR "lib")
+  set(INSTALL_ARCHIVE_DIR  ${CMAKE_INSTALL_LIBDIR})
 endif()
 if(NOT INSTALL_INCLUDE_DIR)
-  set(INSTALL_INCLUDE_DIR "include")
+  set(INSTALL_INCLUDE_DIR ${CMAKE_INSTALL_INCLUDEDIR})
 endif()
