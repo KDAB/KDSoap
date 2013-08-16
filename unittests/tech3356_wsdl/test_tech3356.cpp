2c8dc9e32a290ec40c912df0eb8c95321369c807
Stephen Kelly <stephen.kelly@kdab.com>
2013-08-16 10:50:55 +0200

    Update license to LGPL 2.1.
    Also update the preamble in source headers. Previously it referred to
    the 'Lesser General Public License version 2', which is ambiguous. The
    'Lesser' GPL is version 2.1. Version 2 is the 'Library' GPL.
    
    The kdwsdl2cpp library which was imported from the kdepim repo
    is left unchanged as 'LGPL2+'.

--- unittests/tech3356_wsdl/test_tech3356.cpp
+++ unittests/tech3356_wsdl/test_tech3356.cpp
@@ -9,7 +9,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU Lesser General Public License version 2 and version 3 as published by the
+** GNU Lesser General Public License version 2.1 and version 3 as published by the
 ** Free Software Foundation and appearing in the file LICENSE.LGPL.txt included.
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
