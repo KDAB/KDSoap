d4370fc29844dad894da4a39a15fc90c9a010173
Ryan van der Bijl <ryan@enzosystems.com>
2019-02-01 14:54:08 +0100

    - Fix for coding style, as requested by David

--- kdwsdl2cpp/src/converter_complextype.cpp
+++ kdwsdl2cpp/src/converter_complextype.cpp
@@ -644,7 +644,9 @@ void Converter::createComplexTypeSerializer(KODE::Class &newClass, const XSD::Co
         bool first = true;
         Q_FOREACH (const XSD::Attribute &attribute, attributes) {
             const QString attrName = attribute.name();
-            if (attrName.isEmpty()) { continue; }
+            if (attrName.isEmpty()) {
+                continue;
+            }
             const QString variableName = QLatin1String("d_ptr->") + KODE::MemberVariable::memberVariableName(attrName);
 
             demarshalCode.addBlock(demarshalNameTest(attribute.type(), attrName, &first));
