2bc7508fea40d3505649422b18d626edb8ebe233
Renato Araujo Oliveira Filho <renato.araujo@kdab.com>
2019-12-03 11:39:41 -0300

    Update conan recipe (#195)
    * Add cmake package info into conan recipe
    
    * Update conanfile
    
    - Add missing license
    - Update author
    - Use kdab default qt flags

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -27,19 +27,31 @@ from conans import ConanFile, CMake, tools
 class KdsoapConan(ConanFile):
     name = "KDSoap"
     version = "1.8.0"
-    license = "https://raw.githubusercontent.com/KDAB/KDSoap/kdsoap-1.8.0-release/LICENSE.txt"
-    author = "Renato Araujo Oliveira Filho renato.araujo@kdab.com"
+    license = ("https://raw.githubusercontent.com/KDAB/KDSoap/kdsoap-{0}-release/LICENSE.txt,"
+               "https://raw.githubusercontent.com/KDAB/KDSoap/kdsoap-{0}-release/LICENSE.AGPL3-modified.txt,"
+               "https://raw.githubusercontent.com/KDAB/KDSoap/kdsoap-{0}-release/LICENSE.GPL.txt,"
+               "https://raw.githubusercontent.com/KDAB/KDSoap/kdsoap-{0}-release/LICENSE.LGPL.txt").format(version)
+    author = "Klaralvdalens Datakonsult AB (KDAB) info@kdab.com"
     url = "https://github.com/KDAB/KDSoap.git"
     description = "KD Soap is a Qt-based client-side and server-side SOAP component."
     generators = "cmake"
 
     def requirements(self):
-        self.requires("Qt/5.11.2@bincrafters/stable")
+        self.requires("qt/5.13.2@kdab/stable")
 
     def source(self):
         git = tools.Git(folder="")
         git.clone(self.url)
         git.checkout("kdsoap-%s-release"%self.version)
+
+    def configure(self):
+        # Use same qt configuration for all kdab packages
+        # ~$ conan create -ks -o qt:qttools=True -o qt:qtsvg=True -o qt:qtdeclarative=True -o qt:qtremoteobjects=True -o qt:qtscxml=True . 5.13.2@kdab/stable
+        self.options["qt"].qtsvg = True
+        self.options["qt"].qtdeclarative = True
+        self.options["qt"].qtremoteobjects = True
+        self.options["qt"].qtscxml = True
+        self.options["qt"].qttools = True
 
     def build(self):
         self.cmake = CMake(self)
@@ -48,3 +60,6 @@ class KdsoapConan(ConanFile):
 
     def package(self):
         self.cmake.install()
+
+    def package_info(self):
+        self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)
