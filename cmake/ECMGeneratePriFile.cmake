0757643b44cb1f7817bdd079f7fb50aad1473dda
Allen Winter <allen.winter@kdab.com>
2020-12-21 16:53:48 -0500

    cmake/* - update against upstream.
    also remove unneeded WriteBasicConfigVersionFile.cmake

--- cmake/ECMGeneratePriFile.cmake
+++ cmake/ECMGeneratePriFile.cmake
@@ -72,30 +72,9 @@
 # Since pre-1.0.0.
 
 #=============================================================================
-# Copyright 2014 David Faure <faure@kde.org>
+# SPDX-FileCopyrightText: 2014 David Faure <faure@kde.org>
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# SPDX-License-Identifier: BSD-3-Clause
 
 # Replicate the logic from KDEInstallDirs.cmake as we can't depend on it
 # Ask qmake if we're using the same prefix as Qt
@@ -161,7 +140,12 @@ function(ECM_GENERATE_PRI_FILE)
 
   # Prepare the right number of "../.." to go from ECM_MKSPECS_INSTALL_DIR to the install prefix
   # This allows to make the generated pri files relocatable (no absolute paths)
-  string(REGEX REPLACE "[^/]+" ".." PRI_ROOT_RELATIVE_TO_MKSPECS ${ECM_MKSPECS_INSTALL_DIR})
+  if (IS_ABSOLUTE ${ECM_MKSPECS_INSTALL_DIR})
+     set(BASEPATH ${CMAKE_INSTALL_PREFIX})
+  else()
+    string(REGEX REPLACE "[^/]+" ".." PRI_ROOT_RELATIVE_TO_MKSPECS ${ECM_MKSPECS_INSTALL_DIR})
+    set(BASEPATH "$$PWD/${PRI_ROOT_RELATIVE_TO_MKSPECS}")
+ endif()
 
   set(PRI_TARGET_BASENAME ${EGPF_BASE_NAME})
   set(PRI_TARGET_LIBNAME ${EGPF_LIB_NAME})
@@ -169,12 +153,12 @@ function(ECM_GENERATE_PRI_FILE)
   if(IS_ABSOLUTE "${EGPF_INCLUDE_INSTALL_DIR}")
       set(PRI_TARGET_INCLUDES "${EGPF_INCLUDE_INSTALL_DIR}")
   else()
-      set(PRI_TARGET_INCLUDES "$$PWD/${PRI_ROOT_RELATIVE_TO_MKSPECS}/${EGPF_INCLUDE_INSTALL_DIR}")
+      set(PRI_TARGET_INCLUDES "${BASEPATH}/${EGPF_INCLUDE_INSTALL_DIR}")
   endif()
   if(IS_ABSOLUTE "${EGPF_LIB_INSTALL_DIR}")
       set(PRI_TARGET_LIBS "${EGPF_LIB_INSTALL_DIR}")
   else()
-      set(PRI_TARGET_LIBS "$$PWD/${PRI_ROOT_RELATIVE_TO_MKSPECS}/${EGPF_LIB_INSTALL_DIR}")
+      set(PRI_TARGET_LIBS "${BASEPATH}/${EGPF_LIB_INSTALL_DIR}")
   endif()
   set(PRI_TARGET_DEFINES "")
 
