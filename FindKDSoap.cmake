42dd86e03080b6c7c0e4667f782151e34e52af57
Allen Winter <allen.winter@kdab.com>
2020-12-21 12:38:16 -0500

    CMake buildsystem - minor cosmetic cleanups

--- FindKDSoap.cmake
+++ FindKDSoap.cmake
@@ -1,10 +1,10 @@
 # - Find KDSoap
 # This module finds if KDSoap is installed.
 #
-# KDSoap_FOUND		- Set to TRUE if KDSoap was found.
-# KDSoap_LIBRARIES	- Path to KDSoap libraries.
-# KDSoap_INCLUDE_DIR	- Path to the KDSoap include directory.
-# KDSoap_CODEGENERATOR	- Path to the KDSoap code generator.
+# KDSoap_FOUND         - Set to TRUE if KDSoap was found.
+# KDSoap_LIBRARIES     - Path to KDSoap libraries.
+# KDSoap_INCLUDE_DIR   - Path to the KDSoap include directory.
+# KDSoap_CODEGENERATOR - Path to the KDSoap code generator.
 #
 # Copyright (C) 2011-2020 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 
@@ -12,15 +12,17 @@
 
 include(FindPackageHandleStandardArgs)
 
-find_library(KDSoap_LIBRARIES 
-	NAMES KDSoap kdsoap 
-	PATH_SUFFIXES bin)
-find_path(KDSoap_INCLUDE_DIR 
-   NAMES KDSoapClient/KDSoapValue.h
-   PATH_SUFFIXES include src)
+find_library(KDSoap_LIBRARIES
+  NAMES KDSoap kdsoap
+  PATH_SUFFIXES bin)
+
+find_path(KDSoap_INCLUDE_DIR
+  NAMES KDSoapClient/KDSoapValue.h
+  PATH_SUFFIXES include src)
+
 find_program(KDSoap_CODEGENERATOR
-	NAMES kdwsdl2cpp
-	PATH_SUFFIXES bin)
+  NAMES kdwsdl2cpp
+  PATH_SUFFIXES bin)
 
 mark_as_advanced(KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
 
--- KDSoapMacros.cmake
+++ KDSoapMacros.cmake
@@ -4,14 +4,14 @@
 
 macro(KDSOAP_GENERATE_WSDL _sources)
   set(KDWSDL2CPP kdwsdl2cpp)
-  if (KDSOAP_KDWSDL2CPP_COMPILER)
+  if(KDSOAP_KDWSDL2CPP_COMPILER)
     set(KDWSDL2CPP ${KDSOAP_KDWSDL2CPP_COMPILER})
-  elseif (TARGET KDSoap::kdwsdl2cpp)
+  elseif(TARGET KDSoap::kdwsdl2cpp)
     set(KDWSDL2CPP KDSoap::kdwsdl2cpp)
   endif()
   set(_KSWSDL2CPP_OPTION)
   if(KSWSDL2CPP_OPTION)
-     set(_KSWSDL2CPP_OPTION ${KSWSDL2CPP_OPTION})
+    set(_KSWSDL2CPP_OPTION ${KSWSDL2CPP_OPTION})
   endif()
   foreach(_source_FILE ${ARGN})
     get_filename_component(_tmp_FILE ${_source_FILE} ABSOLUTE)
@@ -19,20 +19,20 @@ macro(KDSOAP_GENERATE_WSDL _sources)
     set(_header_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.h)
     set(_source_wsdl_FILE ${CMAKE_CURRENT_BINARY_DIR}/wsdl_${_basename}.cpp)
     add_custom_command(OUTPUT ${_header_wsdl_FILE}
-       COMMAND ${KDWSDL2CPP}
-       ARGS ${_KSWSDL2CPP_OPTION} ${_tmp_FILE} -o ${_header_wsdl_FILE}
-       MAIN_DEPENDENCY ${_tmp_FILE}
-       DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
+      COMMAND ${KDWSDL2CPP}
+      ARGS ${_KSWSDL2CPP_OPTION} ${_tmp_FILE} -o ${_header_wsdl_FILE}
+      MAIN_DEPENDENCY ${_tmp_FILE}
+      DEPENDS ${_tmp_FILE} ${KDWSDL2CPP})
 
     add_custom_command(OUTPUT ${_source_wsdl_FILE}
-       COMMAND ${KDWSDL2CPP}
-       ARGS ${_KSWSDL2CPP_OPTION} -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
-       MAIN_DEPENDENCY ${_tmp_FILE} ${_header_wsdl_FILE}
-       DEPENDS ${_tmp_FILE} ${KDWSDL2CPP} )
+      COMMAND ${KDWSDL2CPP}
+      ARGS ${_KSWSDL2CPP_OPTION} -impl ${_header_wsdl_FILE} ${_tmp_FILE} -o ${_source_wsdl_FILE}
+      MAIN_DEPENDENCY ${_tmp_FILE} ${_header_wsdl_FILE}
+      DEPENDS ${_tmp_FILE} ${KDWSDL2CPP})
 
     set_source_files_properties(${_header_wsdl_FILE} ${_source_wsdl_FILE} PROPERTIES SKIP_AUTOMOC ON)
     qt5_wrap_cpp(_sources_MOCS ${_header_wsdl_FILE})
     list(APPEND ${_sources} ${_header_wsdl_FILE} ${_source_wsdl_FILE} ${_sources_MOCS})
-  endforeach ()
+  endforeach()
 endmacro()
 
