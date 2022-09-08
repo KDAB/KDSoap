5f254c73b2fde25fe80035738c2ab52c7bd99785
Allen Winter <allen.winter@kdab.com>
2022-09-08 09:08:28 -0400

    cmake-format the entire project
    TODO: cmake-lint

--- FindKDSoap.cmake
+++ FindKDSoap.cmake
@@ -13,18 +13,30 @@
 
 include(FindPackageHandleStandardArgs)
 
-find_library(KDSoap_LIBRARIES
-  NAMES KDSoap kdsoap
-  PATH_SUFFIXES bin)
+find_library(
+    KDSoap_LIBRARIES
+    NAMES KDSoap kdsoap
+    PATH_SUFFIXES bin
+)
 
-find_path(KDSoap_INCLUDE_DIR
-  NAMES KDSoapClient/KDSoapValue.h
-  PATH_SUFFIXES include src)
+find_path(
+    KDSoap_INCLUDE_DIR
+    NAMES KDSoapClient/KDSoapValue.h
+    PATH_SUFFIXES include src
+)
 
-find_program(KDSoap_CODEGENERATOR
-  NAMES kdwsdl2cpp
-  PATH_SUFFIXES bin)
+find_program(
+    KDSoap_CODEGENERATOR
+    NAMES kdwsdl2cpp
+    PATH_SUFFIXES bin
+)
 
 mark_as_advanced(KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
 
-find_package_handle_standard_args(KDSoap DEFAULT_MSG KDSoap_LIBRARIES KDSoap_INCLUDE_DIR KDSoap_CODEGENERATOR)
+find_package_handle_standard_args(
+    KDSoap
+    DEFAULT_MSG
+    KDSoap_LIBRARIES
+    KDSoap_INCLUDE_DIR
+    KDSoap_CODEGENERATOR
+)
