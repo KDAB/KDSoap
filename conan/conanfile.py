7660cc3c12ca41341c38e6ca0c0154612ea68ad3
Allen Winter <allen.winter@kdab.com>
2022-09-14 09:54:43 -0400

    open for a possible 2.1.2 release

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -10,7 +10,7 @@ from conans import ConanFile, CMake, tools
 
 class KdsoapConan(ConanFile):
     name = "KDSoap"
-    version = "2.1.1"
+    version = "2.1.2"
     license = ("https://raw.githubusercontent.com/KDAB/KDSoap/master/LICENSES/MIT.txt")
     author = "Klaralvdalens Datakonsult AB (KDAB) info@kdab.com"
     url = "https://github.com/KDAB/KDSoap.git"
